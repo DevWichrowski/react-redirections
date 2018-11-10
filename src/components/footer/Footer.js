2a4b299e73f1228afbb446087f6ffd0a9f472be5
devwichrowski <devwichrowski@gmail.com>
2018-11-10 23:47:22 +0100

    Modified App.js code, created Main components and moved all code from App.js to Main.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-  return (
-      
-    <div className="phantom">
-    <div className="style">
-      <p>Created by <a href='https://github.com/DevWichrowski'>DevWichrowski</a></p>
-      </div>
-    </div>
-  )
+	return (
+		<div className="phantom">
+			<div className="style">
+				<p>
+					Created by <a href="https://github.com/DevWichrowski">DevWichrowski</a>
+				</p>
+			</div>
+		</div>
+	);
 }
