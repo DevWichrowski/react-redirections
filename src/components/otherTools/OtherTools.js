aa75d14f67ab9a6129bfff721bc5abbb2b5f6241
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-11-11 22:39:34 +0100

    Add styling for OtherTools component.

--- src/components/otherTools/OtherTools.js
+++ src/components/otherTools/OtherTools.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './OtherTools.css';
 
 export default class OtherTools extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Other Tools</h1>
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+	render() {
+		return (
+			<div className="page-body">
+				<h1 className="h1-tools">Other Tools</h1>
+				<hr />
+				<div className="element">
+        <p>lorem ipsum</p>
+					<div className="inputs">
+          <input type="text" class="form-control"/>
+          <input type="text" class="form-control"/>
+					</div>
+					<button id="result-textarea" className="btn btn-danger">
+					Generuj
+				</button>
+				</div>
+				<div className="element">
+        <p>lorem ipsum</p>
+					<div className="inputs">
+          <input type="text" class="form-control"/>
+          <input type="text" class="form-control"/>
+					</div>
+					<button id="result-textarea" className="btn btn-danger">
+					Generuj
+				</button>
+				</div>
+				<div className="element">
+        <p>lorem ipsum</p>
+					<div className="inputs">
+          <input type="text" class="form-control"/>
+          <input type="text" class="form-control"/>
+					</div>
+          <button id="result-textarea" className="btn btn-danger">
+					Generuj
+				</button>
+				</div>
+			</div>
+		);
+	}
 }
