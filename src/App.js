9cc9ef3d6ae0041d5e031387d0429fa1e33bf80f
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-28 20:07:38 +0100

    Add packages and App.scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './styles/app/App.css';
 import Main from './components/Main/Main';
