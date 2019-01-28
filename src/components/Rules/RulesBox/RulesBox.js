4df474c966e729bc35debf46c2e8ba349b0f8c0f
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-28 21:56:07 +0100

    Refactored RulesBox to smart component

--- src/components/Rules/RulesBox/RulesBox.js
+++ src/components/Rules/RulesBox/RulesBox.js
@@ -1,47 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './RulesBox.scss';
 
-export default function RulesBox(props) {
-	return (
-		<div>
-			<div className="page-body">
-				<div className="summary-index-php">
-					<button
-						className="btn btn-danger"
-						type="button"
-						data-toggle="collapse"
-						data-target={`#${props.stateName}`}
-						aria-expanded="false"
-						aria-controls={props.stateName}
-					>
-						{props.description}
-					</button>
-					<div className="collapse" id={props.stateName}>
-					<div className="card card-body">
-							<p>{props.info}</p>
-						<div className="inputs">
+
+class RulesBox extends Component {
+	render() {
+		return (
+			<div>
+				<div className="page-body">
+					<div className="summary-index-php">
+						<button
+							className="btn btn-danger"
+							type="button"
+							data-toggle="collapse"
+							data-target={`#${this.props.stateName}`}
+							aria-expanded="false"
+							aria-controls={this.props.stateName}
+						>
+							{this.props.description}
+						</button>
+						<div className="collapse" id={this.props.stateName}>
+							<div className="card card-body">
+								<p>{this.props.info}</p>
+								<div className="inputs">
 							<textarea
 								type="text"
 								className="form-control"
 								placeholder="Tutaj wklej adres:"
-								onChange={props.saveUrl}
+								onChange={this.props.saveUrl}
 							/>
-							<textarea
-								type="text"
-								className="form-control"
-								placeholder="Tutaj pokaże się wygenerowana reguła"
-								value={props.resultRedirection}
-								readOnly={true}
-							/>
-							<button type="button" className="btn btn-light push-right">Kopiuj</button>
-						</div>
-						<button id="result-textarea" className="btn btn-danger" onClick={props.generateRedirection}>
-							Generuj
-						</button>
+									<textarea
+										type="text"
+										className="form-control"
+										placeholder="Tutaj pokaże się wygenerowana reguła"
+										value={this.props.resultRedirection}
+										readOnly={true}
+									/>
+									<button type="button" className="btn btn-light push-right">Kopiuj</button>
+								</div>
+								<button id="result-textarea" className="btn btn-danger" onClick={this.props.generateRedirection}>
+									Generuj
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
+
+export default RulesBox;
